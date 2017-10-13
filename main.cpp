d7673f354cb893fea387edbcbd3f1f23d65c8196
ApalloAltoBluck <30906123+ApalloAltoBluck@users.noreply.github.com>
2017-10-13 19:53:49 -0400

    Updated main.css

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <string>
+# <fstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main()
+{
+    char response;
+    int v1;
+    cout << "Would you like to roll the dice? Y or N" << endl;
+    cin >> response;
+    srand (time(NULL));
+    if (response == 'y') {
+        v1 = rand() % 6 + 1;
+        cout << v1 << endl;
+        if (v1 == 4|| v1 == 5 || v1 == 6) {
+            cout << "Wow what a roll!" << endl;
+        } else {
+
+            cout << "What a bad a roll" << endl;
+            cout << "Wanna try again?" << endl;
+            if (response == 'y')
+            {
+
+            }
+        }
+
+
+    }
+    else
+    {
+        cout << "Okay!" << endl;
+    }
     return 0;
+
 }
+
+
+
