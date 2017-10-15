75d088ff2a3fa117baf4367d16b6bb2ba7530555
ApalloAltoBluck <30906123+ApalloAltoBluck@users.noreply.github.com>
2017-10-14 21:09:29 -0400

    added restart

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
-# <fstream>
+#include <fstream>
+#include <time.h>
 
 using namespace std;
 
@@ -9,33 +10,42 @@ int main()
 {
     char response;
     int v1;
+
     cout << "Would you like to roll the dice? Y or N" << endl;
     cin >> response;
     srand (time(NULL));
-    if (response == 'y') {
+    while (response == 'y') {
         v1 = rand() % 6 + 1;
         cout << v1 << endl;
         if (v1 == 4|| v1 == 5 || v1 == 6) {
             cout << "Wow what a roll!" << endl;
-        } else {
-
-            cout << "What a bad a roll" << endl;
             cout << "Wanna try again?" << endl;
             if (response == 'y')
             {
 
             }
+            if (response == 'n'){
+
+                cout << "Thanks for trying!" << endl;
+            }
+        } else {
+
+            cout << "What a bad a roll" << endl;
+            cout << "Wanna try again?" << endl;
+            cin >> response;
+            if (response == 'y')
+            {
+
+            }
+            if (response == 'n'){
+
+                cout << "Thanks for trying!" << endl;
+                return 0;
+            }
         }
 
 
     }
-    else
-    {
-        cout << "Okay!" << endl;
-    }
-    return 0;
+
 
 }
-
-
-
